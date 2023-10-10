0ab7091f3bda92796f8250be7c904482ce831cfb
abutler911 <abutler911@gmail.com>
2023-10-09 21:30:30 -0600

    adding timer

--- script.js
+++ script.js
@@ -1,53 +1,118 @@
-const affirmations = [
-  "I worry I'm not a strong leader in the cockpit.",
-  "I feel uneasy when my teaching methods don't seem effective.",
-  "I doubt my ability to mentor aspiring pilots.",
-  "I lack confidence in my leadership skills.",
-  "I fear I can't make quick, correct decisions under pressure.",
-  "I wonder if I'm cut out for this responsibility.",
-  "I fear my crew or students won't respect me.",
-  "I question if I'm good enough to lead and teach.",
-  "I want to be a confident and effective leader.",
-  "I aim to be an inspiring instructor.",
-  "I aspire to be a helpful mentor.",
-  "I want to believe in my abilities.",
-  "I want to trust my decision-making skills.",
-  "I want this role to be a perfect fit for me.",
-  "I want to earn the respect and trust of my crew and students.",
-  "I want to feel I am more than capable.",
-  "I give myself permission to excel in flight leadership.",
-  "I give myself permission to be an effective instructor.",
-  "I grant myself the right to mentor.",
-  "I allow myself to be confident.",
-  "I give myself permission to trust my instincts.",
-  "I allow myself to embrace this role.",
-  "I give myself permission to be respected.",
-  "I grant myself the right to feel competent and capable.",
-  "I am grateful for each flight where I lead successfully.",
-  "I appreciate every student who learns from me.",
-  "I'm thankful for the opportunity to mentor and make a difference.",
-  "I am becoming more confident every day.",
-  "I value the quick decisions I've made that were correct.",
-  "I take pride in my growth and responsibility.",
-  "I relish the respect I earn from my crew and students.",
-  "I'm proud of the leader, instructor, and mentor I am becoming. I am enough.",
+const affirmationSections = [
+  {
+    section: "Addressing Insecurities",
+    phrases: [
+      "I worry I'm not a strong leader in the cockpit.",
+      "I feel uneasy when my teaching methods don't seem effective.",
+      "I doubt my ability to mentor aspiring pilots.",
+      "I lack confidence in my leadership skills.",
+      "I fear I can't make quick, correct decisions under pressure.",
+      "I wonder if I'm cut out for this responsibility.",
+      "I fear my crew or students won't respect me.",
+      "I question if I'm good enough to lead and teach.",
+    ],
+  },
+  {
+    section: "Shifting to Desires",
+    phrases: [
+      "I want to be a confident and effective leader.",
+      "I aim to be an inspiring instructor.",
+      "I aspire to be a helpful mentor.",
+      "I want to believe in my abilities.",
+      "I want to trust my decision-making skills.",
+      "I want this role to be a perfect fit for me.",
+      "I want to earn the respect and trust of my crew and students.",
+      "I want to feel I am more than capable.",
+    ],
+  },
+  {
+    section: "Giving Permission",
+    phrases: [
+      "I give myself permission to excel in flight leadership.",
+      "I give myself permission to be an effective instructor.",
+      "I grant myself the right to mentor.",
+      "I allow myself to be confident.",
+      "I give myself permission to trust my instincts.",
+      "I allow myself to embrace this role.",
+      "I give myself permission to be respected.",
+      "I grant myself the right to feel competent and capable.",
+    ],
+  },
+  {
+    section: "Affirmations and Gratitude",
+    phrases: [
+      "I am grateful for each flight where I lead successfully.",
+      "I appreciate every student who learns from me.",
+      "I'm thankful for the opportunity to mentor and make a difference.",
+      "I am becoming more confident every day.",
+      "I value the quick decisions I've made that were correct.",
+      "I take pride in my growth and responsibility.",
+      "I relish the respect I earn from my crew and students.",
+      "I'm proud of the leader, instructor, and mentor I am becoming. I am enough.",
+    ],
+  },
 ];
 
-let currentIndex = 0;
+let currentSectionIndex = 0;
+let currentPhraseIndex = 0;
 let isPaused = false;
+let countdown;
+let countdownValue = 5;
 
 const displayElement = document.getElementById("affirmation-display");
+const timerElement = document.getElementById("timer");
 const prevButton = document.getElementById("prev-button");
 const nextButton = document.getElementById("next-button");
 const pauseButton = document.getElementById("pause-button");
 
 function updateDisplay() {
-  displayElement.textContent = affirmations[currentIndex];
+  if (
+    currentPhraseIndex < affirmationSections[currentSectionIndex].phrases.length
+  ) {
+    displayElement.textContent =
+      affirmationSections[currentSectionIndex].phrases[currentPhraseIndex];
+  } else {
+    displayElement.textContent =
+      "Place a hand on your heart and belly, and take three deep breaths.";
+  }
+  resetTimer();
 }
 
+function resetTimer() {
+  clearInterval(countdown);
+  countdownValue = 5; // Reset timer to 5 seconds
+  updateTimer();
+  countdown = setInterval(() => {
+    countdownValue--;
+    updateTimer();
+    if (countdownValue <= 0) {
+      clearInterval(countdown);
+      moveToNext(); // Move to the next affirmation
+    }
+  }, 1000);
+}
+
+function updateTimer() {
+  timerElement.textContent = countdownValue ? `${countdownValue}s` : "";
+}
 function moveToNext() {
-  if (!isPaused && currentIndex < affirmations.length - 1) {
-    currentIndex++;
+  if (!isPaused) {
+    if (
+      currentPhraseIndex <
+      affirmationSections[currentSectionIndex].phrases.length
+    ) {
+      currentPhraseIndex++;
+    } else {
+      currentSectionIndex++;
+      currentPhraseIndex = 0;
+    }
+
+    if (currentSectionIndex >= affirmationSections.length) {
+      // Reset if reached the end
+      currentSectionIndex = 0;
+      currentPhraseIndex = 0;
+    }
+
     updateDisplay();
   }
 }
