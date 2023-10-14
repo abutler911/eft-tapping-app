9b1a25e1829701742153e9dd95fc84f17a825f5e
Andrew Butler <abutler911@gmail.com>
2023-10-14 15:03:57 -0700

    added new user model

--- models/User.js
+++ models/User.js
@@ -4,9 +4,9 @@ const bcrypt = require("bcrypt");
 const UserSchema = new mongoose.Schema({
   username: String,
   password: String,
+  affirmations: [String],
 });
 
-// Hash the password before saving it to the database
 UserSchema.pre("save", function (next) {
   const user = this;
   if (!user.isModified("password")) return next();
