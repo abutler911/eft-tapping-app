5839d4d2924d8c51f48b50ef21f990449ea857d3
abutler911 <abutler911@gmail.com>
2023-10-16 16:06:07 -0600

    added db connection

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
+const connectDB = require("./config/db");
+const indexRoutes = require("./routes/indexRoutes");
 
 const app = express();
 const port = 3000;
+
+// Connect to MongoDB
+connectDB();
 
 // Setting EJS as the view engine
 app.set("view engine", "ejs");
@@ -10,10 +16,8 @@ app.set("view engine", "ejs");
 // Setting up the public folder to serve static files
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-  // Render an EJS view file
-  res.render("index", { message: "Hello World!" });
-});
+// Use routes
+app.use("/", indexRoutes);
 
 app.listen(port, () => {
   console.log(`Emotional Journey App listening on http://localhost:${port}`);
