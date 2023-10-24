ed77a204bfca9a8f2d6c5c1916f55ade35b82f5b
Andrew Butler <abutler911@gmail.com>
2023-10-23 21:03:33 -0600

    changing stuff

--- routes/affirmationRoutes.js
+++ routes/affirmationRoutes.js
@@ -2,14 +2,38 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 
+function camelCaseToSpaces(str) {
+  return str.replace(/([A-Z])/g, " $1").toLowerCase();
+}
+
 router.get("/affirmation-setup", async (req, res) => {
   let user = req.user;
+
   if (!user || !user.affirmations) {
     const userId = req.session.passport.user;
     user = await User.findById(userId);
+    user = user ? JSON.parse(JSON.stringify(user)) : null; // Deep clone to plain object
   }
 
-  res.render("affirmation-setup", { user: user.toObject() });
+  // Debugging Step 1: Check the Conversion
+  console.log(
+    "Is Plain Object?",
+    user instanceof Object &&
+      !(user instanceof Array) &&
+      !(user instanceof Function)
+  );
+
+  const categoriesWithSpaces = {};
+  if (user && user.affirmations) {
+    for (const [key, value] of Object.entries(user.affirmations)) {
+      categoriesWithSpaces[camelCaseToSpaces(key)] = value;
+    }
+  }
+
+  // Debugging Step 2: Check the Data
+  console.log("Categories with Spaces:", categoriesWithSpaces);
+
+  res.render("affirmation-setup", { user, categoriesWithSpaces });
 });
 
 router.get("/enter-affirmations", (req, res) => {
